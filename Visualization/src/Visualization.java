08a1e7df03d5011dabca1aff2594652760557dbf
Sachi <89476951+sob505@users.noreply.github.com>
2023-09-03 17:46:32 -0600

    Added Javadoc and replaced step with multiplicand for more cohesion between animation and sliders.

--- Visualization/src/Visualization.java
+++ Visualization/src/Visualization.java
@@ -1,3 +1,14 @@
+/*
+    Sachi Barnaby
+    Project 1: Visualization
+
+    This project creates a GUI to visualize times tables using a circle and lines.
+    To run it, just run Visualization.main(). You can use each of the sliders to control different parts of the
+    visualization (as indicated in text), and you can use the button to stop or start the animation. The animation
+    automatically cycles through 72 different colors by changing the hue value in HSB (see findColors()).
+
+ */
+
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
@@ -12,8 +23,6 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -22,14 +31,12 @@ public class Visualization extends Application {
     private final Canvas canvas = new Canvas(800,500);
     private final GraphicsContext gc = canvas.getGraphicsContext2D();
     private Timeline animation;
-    private double step = 1;
-    private double oldMultiplicand = 2;
-    private double multiplicand = 2;
+    private double multiplicand = 0;
     private int numPoints = 100;
     private double framerate = 1000; // in milliseconds
     private double increment = 1;
 
-    private final List<Color> colors = findColors(); //allColors();
+    private final List<Color> colors = findColors();
     private int colorIndex = 0;
 
     public static void main(String[] args) {
@@ -40,16 +47,16 @@ public class Visualization extends Application {
     public void start(Stage stage) {
         stage.setTitle("Visualization");
 
+        // Set up the main circle in the canvas
         int[] offset = {250,50};
         double diameter = 300;
         drawCircle(offset, diameter);
 
-
         // Create a slider so user can change which times table is used
-        Slider multiplicandSlider = getSlider(0.0,360.0,0);
+        Slider multiplicandSlider = getSlider(0.0,360.0,multiplicand);
 
         // Create a slider so user can change how many points are on the circle
-        Slider pointsSlider = getSlider(0,360,0);
+        Slider pointsSlider = getSlider(0,360,numPoints);
 
         // Create a slider so user can control number of frames per second
         Slider framerateSlider = getSlider(0, 2000, framerate);
@@ -87,12 +94,6 @@ public class Visualization extends Application {
         incrementSlider.valueProperty().addListener((observableValue, oldValue, newValue) ->
                 increment = incrementSlider.getValue());
 
-        // Set the preferred sizes of the sliders
-        multiplicandSlider.setPrefWidth(400);
-        pointsSlider.setPrefWidth(400);
-        framerateSlider.setPrefWidth(400);
-        incrementSlider.setPrefWidth(400);
-
         // Create Labels for the sliders
         Label multiplicandLabel = new Label("Change the multiplicand:");
         Label pointsLabel = new Label("Change the number of points around the circle:");
@@ -111,28 +112,8 @@ public class Visualization extends Application {
                 framerateSlider, framerateLabel, incrementSlider, incrementLabel, btn);
 
         // Position the sliders and labels within the Pane
-        multiplicandSlider.setTranslateX(350);
-        multiplicandSlider.setTranslateY(550);
-        multiplicandLabel.setTranslateX(50);
-        multiplicandLabel.setTranslateY(550);
-
-        pointsSlider.setTranslateX(350);
-        pointsSlider.setTranslateY(500);
-        pointsLabel.setTranslateX(50);
-        pointsLabel.setTranslateY(500);
-
-        framerateSlider.setTranslateX(350);
-        framerateSlider.setTranslateY(450);
-        framerateLabel.setTranslateX(50);
-        framerateLabel.setTranslateY(450);
-
-        incrementSlider.setTranslateX(350);
-        incrementSlider.setTranslateY(400);
-        incrementLabel.setTranslateX(50);
-        incrementLabel.setTranslateY(400);
-
-        btn.setTranslateX(600);
-        btn.setTranslateY(300);
+        positionElements(multiplicandSlider,multiplicandLabel,pointsSlider,pointsLabel,framerateSlider,framerateLabel,
+                incrementSlider,incrementLabel,btn);
 
         // Create a StackPane to overlay the Pane (canvas + slider)
         StackPane stackPane = new StackPane();
@@ -147,6 +128,7 @@ public class Visualization extends Application {
         animation.play();
     }
 
+    // Set up the Pause/Play button
     private Button getButton() {
         Button btn = new Button();
         btn.setText ("Pause");
@@ -157,12 +139,7 @@ public class Visualization extends Application {
             if (Objects.equals(btn.getText(), "Pause")) {
                 animation.pause();
                 btn.setText("Play");
-                oldMultiplicand = multiplicand;
             } else {
-                // If the multiplicand has changed since the button was pressed, restart the animation at that value
-                if (oldMultiplicand != multiplicand) {
-                    step = multiplicand;
-                }
                 animation.play();
                 btn.setText("Pause");
             }
@@ -170,13 +147,15 @@ public class Visualization extends Application {
         return btn;
     }
 
-    private Slider getSlider(double min, double max, double start) {
+    // Set up a slider
+    private static Slider getSlider(double min, double max, double start) {
         Slider slider = new Slider(min,max,start);
         slider.setShowTickLabels(true);
         slider.setShowTickMarks(true);
         slider.setMajorTickUnit(40.0);
 
-        slider.setPrefWidth(200);
+        // Set the preferred sizes of the sliders
+        slider.setPrefWidth(400);
 
         return slider;
     }
@@ -204,18 +183,20 @@ public class Visualization extends Application {
     }
 
     private void drawLines(int numPoints, double multiplicand, double[][] coordinates) {
-        //int random = (int) (Math.random() * colors.size());
-        Color choice = colors.get(colorIndex);//random);
+        // Circle through all the color options in the list of colors
+        Color choice = colors.get(colorIndex);
         colorIndex++;
         if(colorIndex >= colors.size()) { colorIndex = 0; }
         gc.setStroke(choice);
 
+        // Then draw the lines between each of the coordinates
         for(int m = 0; m < numPoints; m++) {
             int remainder = (int) ((multiplicand * m) % numPoints);
             gc.strokeLine(coordinates[m][0], coordinates[m][1], coordinates[remainder][0], coordinates[remainder][1]);
         }
     }
 
+    // Generate a list of colors using HSB
     private static List<Color> findColors() {
         List<Color> colors = new ArrayList<>();
         for(int hue = 0; hue < 360; hue += 5) {
@@ -224,37 +205,52 @@ public class Visualization extends Application {
         return colors;
     }
 
-    // Found this on: https://stackoverflow.com/questions/17464906/how-to-list-all-colors-in-javafx
-    private static List<Color> allColors() throws ClassNotFoundException, IllegalAccessException {
-        List<Color> colors = new ArrayList<>();
-        Class<?> clazz = Class.forName("javafx.scene.paint.Color");
-        Field[] field = clazz.getFields();
-        for (Field f : field) {
-            Object obj = f.get(null);
-            if (obj instanceof Color) {
-                colors.add((Color) obj);
-            }
-        }
-        return colors;
+    // Position all the sliders, labels, and the button within the frame
+    private static void positionElements(Slider ms, Label ml, Slider ps, Label pl, Slider fs, Label fl, Slider is, Label il,
+                                  Button btn) {
+        ms.setTranslateX(350);
+        ms.setTranslateY(550);
+        ml.setTranslateX(50);
+        ml.setTranslateY(550);
+
+        ps.setTranslateX(350);
+        ps.setTranslateY(500);
+        pl.setTranslateX(50);
+        pl.setTranslateY(500);
+
+        fs.setTranslateX(350);
+        fs.setTranslateY(450);
+        fl.setTranslateX(50);
+        fl.setTranslateY(450);
+
+        is.setTranslateX(350);
+        is.setTranslateY(400);
+        il.setTranslateX(50);
+        il.setTranslateY(400);
+
+        btn.setTranslateX(600);
+        btn.setTranslateY(300);
     }
 
+    // Run the animation using the class attributes
     private void runAnimation(int[] offset, double diameter, double increment){
-        step += increment;
-        if(step > 360) { step = 0; }
+        multiplicand += increment;
+        if(multiplicand > 360) { multiplicand = 0; }
         // Create a 2D array to store the coordinates of numPoints points around a circle
         double[][] coordinates = getPoints(numPoints, offset, diameter);
-
+        // Redraw the circle and lines with the changed value
         gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
         drawCircle(offset, diameter);
-        drawLines(numPoints, step, coordinates);
+        drawLines(numPoints, multiplicand, coordinates);
     }
 
+    // When the slider is moved, change the corresponding value and pause the animation
     private void updateSlider(Button btn, int[] offset, double diameter) {
         animation.pause();
         btn.setText("Play");
         // Create a 2D array to store the coordinates of numPoints points around a circle
         double[][] coordinates = getPoints(numPoints, offset, diameter);
-
+        // Redraw the circle and lines with the changed value
         gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
         drawCircle(offset, diameter);
         drawLines(numPoints, multiplicand, coordinates);
